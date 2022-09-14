0bdab98d366083ec011048e35d4606390866feb6
oanna117 <o.anan117@gmail.com>
2022-09-15 08:23:40 +0900

    未完了タスクの表示まで完成

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// 接続に必要な情報を定数として定義
+define('DSN', 'mysql:host=db;dbname=task_app;charset=utf8');
+define('USER', 'task_admin');
+define('PASSWORD', '1234');
+
+// doneの状態を定数として定義
+define('TASK_NOTYET', 0);
+define('TASK_DONE', 1); 
+
+
+// 定数を定義するときは基本大文字で
